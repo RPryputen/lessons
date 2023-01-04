f05e6b21ee9f6d60e081f83822dd79d59ccb25a7
RPryputen <108124300+RPryputen@users.noreply.github.com>
2023-01-05 00:32:53 +0200

    Add files via upload

--- dist/js/app.js
+++ dist/js/app.js
@@ -14,6 +14,26 @@
             document.documentElement.classList.add(className);
         }));
     }
+    let isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+        }
+    };
     let _slideUp = (target, duration = 500, showmore = 0) => {
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
@@ -212,6 +232,42 @@
     function functions_menuClose() {
         bodyUnlock();
         document.documentElement.classList.remove("menu-open");
+    }
+    function customCursor(isShadowTrue) {
+        const wrapper = document.querySelector("[data-custom-cursor]") ? document.querySelector("[data-custom-cursor]") : document.documentElement;
+        if (wrapper && !isMobile.any()) {
+            const cursor = document.createElement("div");
+            cursor.classList.add("fls-cursor");
+            cursor.style.opacity = 0;
+            cursor.insertAdjacentHTML("beforeend", `<span class="fls-cursor__pointer"></span>`);
+            isShadowTrue ? cursor.insertAdjacentHTML("beforeend", `<span class="fls-cursor__shadow"></span>`) : null;
+            wrapper.append(cursor);
+            const cursorPointer = document.querySelector(".fls-cursor__pointer");
+            const cursorPointerStyle = {
+                width: cursorPointer.offsetWidth,
+                height: cursorPointer.offsetHeight
+            };
+            let cursorShadow, cursorShadowStyle;
+            if (isShadowTrue) {
+                cursorShadow = document.querySelector(".fls-cursor__shadow");
+                cursorShadowStyle = {
+                    width: cursorShadow.offsetWidth,
+                    height: cursorShadow.offsetHeight
+                };
+            }
+            function mouseActions(e) {
+                if ("mouseout" === e.type) cursor.style.opacity = 0; else if ("mousemove" === e.type) {
+                    cursor.style.removeProperty("opacity");
+                    if (e.target.closest("button") || e.target.closest("a") || e.target.closest("input") || "none" !== window.getComputedStyle(e.target).cursor && "default" !== window.getComputedStyle(e.target).cursor) cursor.classList.add("_hover"); else cursor.classList.remove("_hover");
+                } else if ("mousedown" === e.type) cursor.classList.add("_active"); else if ("mouseup" === e.type) cursor.classList.remove("_active");
+                cursorPointer ? cursorPointer.style.transform = `translate3d(${e.clientX - cursorPointerStyle.width / 2}px, ${e.clientY - cursorPointerStyle.height / 2}px, 0)` : null;
+                cursorShadow ? cursorShadow.style.transform = `translate3d(${e.clientX - cursorShadowStyle.width / 2}px, ${e.clientY - cursorShadowStyle.height / 2}px, 0)` : null;
+            }
+            window.addEventListener("mouseup", mouseActions);
+            window.addEventListener("mousedown", mouseActions);
+            window.addEventListener("mousemove", mouseActions);
+            window.addEventListener("mouseout", mouseActions);
+        }
     }
     function FLS(message) {
         setTimeout((() => {
@@ -4336,6 +4392,7 @@
     isWebp();
     menuInit();
     spollers();
+    customCursor(true);
     formFieldsInit({
         viewPass: true,
         autoHeight: false
